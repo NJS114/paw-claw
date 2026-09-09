036753071f2d55eef74db0d63c6391494dd8de7e
NJS114 <104267084+NJS114@users.noreply.github.com>
2026-09-09 17:22:34 +0200

    Load progression UI styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import './styles.css';
 import './assets.css';
 import './card-images.css';
 import './collection.css';
+import './progression.css';
 import { App } from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
